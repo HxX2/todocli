732da0fb4f7cd96ff187812e6cba3e5200bc8f3e
HxX2 <zaka.hanix@gmail.com>
2024-07-29 03:15:02 +0100

    Added toggle, open editor, add options

--- pkg/todo/op.go
+++ pkg/todo/op.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
 	"strings"
 
 	"github.com/fatih/color"
@@ -64,4 +65,57 @@ func (t Todo) RemTask(taskId int) {
 	file.Write(t.filePath, strings.Join(newLines, "\n"), os.O_TRUNC)
 
 	t.PrintList()
+}
+
+func (t Todo) AddTask(task string) {
+	ftask := fmt.Sprintf("[] %s\n", task)
+
+	file.Write(t.filePath, ftask, os.O_APPEND)
+
+	t.PrintList()
+}
+
+func (t Todo) togleTask(taskId int) {
+	newLines := make([]string, 0)
+	todoFile := file.Open(t.filePath)
+	defer todoFile.Close()
+
+	i := 1
+	scanner := bufio.NewScanner(todoFile)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if i == taskId {
+			if strings.Contains(line, "[X]") {
+				line = strings.Replace(line, "[X", "[", 1)
+			} else if strings.Contains(line, "[]") {
+				line = strings.Replace(line, "[", "[X", 1)
+			}
+		}
+		newLines = append(newLines, line)
+		i++
+	}
+
+	if err := scanner.Err(); err != nil {
+		pprint.Error(fmt.Sprintf("Can't read %s", t.filePath))
+	}
+
+	file.Write(t.filePath, strings.Join(newLines, "\n"), os.O_TRUNC)
+
+	t.PrintList()
+}
+
+func (t Todo) openEditor() {
+	editor := os.Getenv("EDITOR")
+
+	if editor == "" {
+		pprint.Error("Can't open editor [no $EDITOR env]")
+		return
+	}
+
+	err := exec.Command(editor, t.filePath).Run()
+
+	if err != nil {
+		pprint.Error(fmt.Sprintf("Failed to open editor\n%s\n", err))
+		return
+	}
 }
